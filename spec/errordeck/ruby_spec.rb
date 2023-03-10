99c822a308c6c2a23b9318e491fb9a71d3148ff2
Håkan <hakan@dun.se>
2023-03-11 00:07:31 +0100

    Initial commit

---
+++ spec/errordeck/ruby_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Errordeck::Ruby do
+  it "has a version number" do
+    expect(Errordeck::Ruby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
